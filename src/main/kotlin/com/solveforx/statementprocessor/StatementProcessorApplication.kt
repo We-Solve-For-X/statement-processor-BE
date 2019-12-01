9f69530c34f28bdddd235dba484b8ca336bb7d83
Mardu Swanepoel <mardu.swanepoel@spandigital.com>
2019-12-01 10:30:14 +0200

    first commit after setup

---
+++ src/main/kotlin/com/solveforx/statementprocessor/StatementProcessorApplication.kt
@@ -0,0 +1,11 @@
+package com.solveforx.statementprocessor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatementProcessorApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatementProcessorApplication>(*args)
+}
---
+++ src/test/kotlin/com/solveforx/statementprocessor/StatementProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.solveforx.statementprocessor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatementProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
